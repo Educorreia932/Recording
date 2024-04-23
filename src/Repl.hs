7ab960c813bdc98b9fa1ad5e8af9c4f019cd35df
Eduardo Correia <skelozard1@gmail.com>
2024-04-23 18:31:06 +0100

    Added a command to determine type of an expression

--- src/Repl.hs
+++ src/Repl.hs
@@ -3,16 +3,19 @@ module Repl where
 import Control.Monad (forever)
 import Implementation.Compilation
 import Implementation.Evaluator
+import Implicit.TypeInference (typeInference)
 import System.Exit
 import System.IO (hFlush, stdout)
 
 executeCommand :: String -> String -> IO ()
 executeCommand command expression = case command of
     x | x `elem` [":quit", ":q"] -> exitSuccess
-    x | x `elem` [":eval", ":e"] -> print $ evaluate expression
+    x | x `elem` [":eval", ":e"] -> print $ evaluate' $ compile $ fst $ typeInference expression
+    x | x `elem` [":type", ":t"] -> print $ fst $ typeInference expression
     x | x `elem` [":help", ":h"] -> do
         putStrLn ":q[uit] - Exit the REPL"
         putStrLn ":e[val] - Evaluate an expression"
+        putStrLn ":t[ype] - Infer the type of an expression"
         putStrLn ":h[elp] - Display this help message"
     _ -> putStrLn "Invalid command."
 
@@ -21,5 +24,5 @@ repl = forever $ do
     putStr "λ> "
     hFlush stdout
     input <- getLine
-    let (command, expression) = span (/=' ') input
+    let (command, expression) = span (/= ' ') input
     executeCommand command expression

3f69a537b1d8891c2e6838fa71651579597724a7
Eduardo Correia <skelozard1@gmail.com>
2025-03-01 15:07:49 +0000

    Fix: Report of test failures

--- tests/Tests.hs
+++ tests/Tests.hs
@@ -5,13 +5,19 @@ import EvaluatorTest qualified
 import ParserTest qualified
 import Test.HUnit (Test (TestList), runTestTT)
 import TypeInferenceTest qualified
+import Test.HUnit.Base
+import System.Exit (exitFailure, exitSuccess)
 
-main =
-    runTestTT
-        ( TestList
-            [ ParserTest.tests
-            , TypeInferenceTest.tests
-            , CompilationTest.tests
-            , EvaluatorTest.tests
-            ]
-        )
+tests =
+    TestList
+        [ ParserTest.tests
+        , TypeInferenceTest.tests
+        , CompilationTest.tests
+        , EvaluatorTest.tests
+        ]
+
+main = do
+    counts <- runTestTT tests
+    if errors counts + failures counts > 0
+        then exitFailure
+        else exitSuccess
